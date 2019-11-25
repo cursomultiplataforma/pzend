45b78cf6452725fe87ceabae065d640731f38613
Daniel Pérez <daniel.perez@urjc.es>
2019-11-25 16:48:12 +0100

    fix

--- module/Application/src/Controller/UsuarioController.php
+++ module/Application/src/Controller/UsuarioController.php
@@ -1,139 +1,141 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
-namespace Application\Controller;
-
-use Zend\Db\Adapter\Adapter;
-use Zend\Http\Response;
-use Application\Model\Entity\Usuario;
-
-class UsuarioController extends MasterController
-{
-    /** @var Adapter */
-    private $dbAdapter;
-    private $usuario;
     /**
-     * IndexController constructor.
-     * @param $dbAdapter
+     * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+     * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+     * @license   http://framework.zend.com/license/new-bsd New BSD License
      */
-    public function __construct($dbAdapter)
+
+    namespace Application\Controller;
+
+    use Application\Model\Entity\Usuario;
+    use Zend\Db\Adapter\Adapter;
+    use Zend\Http\Response;
+
+    class UsuarioController extends MasterController
     {
-        $this->dbAdapter = $dbAdapter;
-        $this->usuario = new Usuario($this->dbAdapter);
-    }
+        /** @var Adapter */
+        private $dbAdapter;
+        private $usuario;
 
-    public function create($data)
-    {
-        $data =  $this->array_change_key_case_recursive($data, CASE_UPPER);
-        $res = $this->usuario->addData($data);
-        $data =  $this->array_change_key_case_recursive($data, CASE_LOWER);
-
-        $response = $this->getResponse();
-        $response->getHeaders()->addHeaders([
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => '*'
-        ]);
-        $response->setContent(json_encode($data));
-
-        if ($res == true) {
-            $response->setStatusCode(200);
-        } else {
-            $response->setStatusCode(500);
-        }
-        return $response;
-    }
-
-    public function getList()
-    {
-        $data = $this->usuario->getAllData();
-        /*
-         * Por defecto nos devuelve las keys en mayúsuclas
-         * Dos opciones:
-         * - Convertirlo a minúsculas antes de devolverlo
-         * - En el Angular definir los campos de los objetos en mayúsculas
+        /**
+         * IndexController constructor.
+         * @param $dbAdapter
          */
-        $data =  $this->array_change_key_case_recursive($data, CASE_LOWER);
-
-        /** @var Response $response */
-        $response = $this->getResponse();
-        $response->getHeaders()->addHeaders([
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => '*'
-        ]);
-        $response->setContent(json_encode($data));
-        $response->setStatusCode(200);
-        return $response;
-    }
-
-    public function get($id)
-    {
-        $data = $this->usuario->getDataId($id);
-
-        /** @var Response $response */
-        $response = $this->getResponse();
-        $response->getHeaders()->addHeaders([
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => '*'
-        ]);
-        $response->setContent(json_encode([$data]));
-        $response->setStatusCode(200);
-        return $response;
-    }
-
-    public function options()
-    {
-        /** @var Response $response */
-        $response = $this->getResponse();
-        $response->getHeaders()->addHeaders([
-            'Access-Control-Allow-Origin' => "*",
-            'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => ' Origin, Content-Type, X-Auth-Token',
-            'Content-Type' => 'application/json'
-        ]);
-        return $response;
-    }
-
-    public function update($id, $data)
-    {
-        $data =  $this->array_change_key_case_recursive($data, CASE_UPPER);
-        $res = $this->usuario->updateData($id,$data);
-        $data =  $this->array_change_key_case_recursive($data, CASE_LOWER);
-
-        $response = $this->getResponse();
-        $response->getHeaders()->addHeaders([
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => '*'
-        ]);
-        $response->setContent(json_encode($data));
-
-        if ($res == true) {
-            $response->setStatusCode(200);
-        } else {
-            $response->setStatusCode(500);
+        public function __construct($dbAdapter)
+        {
+            $this->dbAdapter = $dbAdapter;
+            $this->usuario = new Usuario($this->dbAdapter);
         }
-        return $response;
-    }
 
-    public function delete($id)
-    {
-        $res = $this->usuario->deleteData($id);
+        public function create($data)
+        {
+            $data = $this->array_change_key_case_recursive($data, CASE_UPPER);
+            $res = $this->usuario->addData($data);
+            $data = $this->array_change_key_case_recursive($data, CASE_LOWER);
 
-        $response = $this->getResponse();
-        $response->getHeaders()->addHeaders([
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => '*'
-        ]);
-        $response->setContent(json_encode($id));
+            $response = $this->getResponse();
+            $response->getHeaders()->addHeaders([
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => '*'
+            ]);
+            $response->setContent(json_encode($data));
 
-        if ($res == true) {
-            $response->setStatusCode(200);
-        } else {
-            $response->setStatusCode(500);
+            if ($res == true) {
+                $response->setStatusCode(200);
+            } else {
+                $response->setStatusCode(500);
+            }
+            return $response;
         }
-        return $response;
+
+        public function getList()
+        {
+            $data = $this->usuario->getAllData();
+            /*
+             * Por defecto nos devuelve las keys en mayúsuclas
+             * Dos opciones:
+             * - Convertirlo a minúsculas antes de devolverlo
+             * - En el Angular definir los campos de los objetos en mayúsculas
+             */
+            $data = $this->array_change_key_case_recursive($data, CASE_LOWER);
+
+            /** @var Response $response */
+            $response = $this->getResponse();
+            $response->getHeaders()->addHeaders([
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => '*'
+            ]);
+            $response->setContent(json_encode($data));
+            $response->setStatusCode(200);
+            return $response;
+        }
+
+        public function get($id)
+        {
+            $data = $this->usuario->getDataId($id);
+
+            /** @var Response $response */
+            $response = $this->getResponse();
+            $response->getHeaders()->addHeaders([
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => '*'
+            ]);
+            $response->setContent(json_encode([$data]));
+            $response->setStatusCode(200);
+            return $response;
+        }
+
+        public function options()
+        {
+            /** @var Response $response */
+            $response = $this->getResponse();
+            $response->getHeaders()->addHeaders([
+                'Access-Control-Allow-Origin' => "*",
+                'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'X-Requested-With, Origin, Content-Type, X-Auth-Token',
+                'Content-Type' => 'application/json'
+            ]);
+            return $response;
+        }
+
+        public function update($id, $data)
+        {
+            $data = $this->array_change_key_case_recursive($data, CASE_UPPER);
+            $res = $this->usuario->updateData($id, $data);
+            $data = $this->array_change_key_case_recursive($data, CASE_LOWER);
+
+            $response = $this->getResponse();
+            $response->getHeaders()->addHeaders([
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => '*'
+            ]);
+            $response->setContent(json_encode($data));
+
+            if ($res == true) {
+                $response->setStatusCode(200);
+            } else {
+                $response->setStatusCode(500);
+            }
+            return $response;
+        }
+
+        public function delete($id)
+        {
+            $response = $this->getResponse();
+            $response->getHeaders()->addHeaders([
+                'Access-Control-Allow-Origin' => "*",
+                'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'X-Requested-With, Origin, Content-Type, X-Auth-Token',
+                'Content-Type' => 'application/json'
+            ]);
+            $response->setContent(json_encode($id));
+            $res = $this->usuario->deleteData($id);
+
+            if ($res == true) {
+                $response->setStatusCode(200);
+            } else {
+                $response->setStatusCode(500);
+            }
+            return $response;
+        }
     }
-}
